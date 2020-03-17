c051b1dec3fe70690b2897bc3a2d6c9104ec215b
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-03-16 22:31:51 -0600

    format layout and create some buttons

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -5,7 +5,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void response() {
+    print('Answer 1 chosen!');
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'), );
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question?'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: response,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('Answer 3 chosen'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
